a1251c8e852f3673007f451d0cbd906796466e9e
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2023-01-06 11:31:44 +0700

    Create text for pc.user.js

---
+++ text for pc.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        js for PC
+// @namespace   SpriteNguyen
+// @match       *://*/*
+// @exclude     *://www.xvideos.com/*
+// @exclude     *://www.codecguide.com/*
+// @exclude     *://www.leagueoflegends.com/*
+// @grant       GM_addStyle
+// @version     0.1.1
+// @author      zNDB
+// @run-at      document-start
+// @icon        https://e7.pngegg.com/pngimages/247/169/png-clipart-high-heeled-shoe-supermodel-scary-beautiful-model-celebrities-fashion.png
+// @description 24/12/2022
+// ==/UserScript==
+
+//GM_addStyle('@font-face{font-family: VeraSeBd; src: url("https://mdn.github.io/css-examples/web-fonts/VeraSeBd.ttf");}');
+//GM_addStyle('@font-face {font-family: byNDB; src: url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');}');
+//GM_addStyle('@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");');
+/*for all*/GM_addStyle('p,.bbWrapper { font-size: 20px !important;}');
+/*voz.vn*/GM_addStyle('.link{color:red;} .bbCodeBlock-expandContent,.fauxBlockLink-blockLink{font-size:16px;} .node-extra-title{font-size:18px;} .js-nodeMain.node-main > .node-title,.structItem-cell--main.structItem-cell > .structItem-title{font-size:20px;} .userTitle,.message-inner > .message-cell--main.message-cell > .js-quickEditTarget.message-main > .js-messageContent.message-content > .message-signature > .bbWrapper{font-size:13px}');
+/*website*/GM_addStyle('/*just vnexpress.net*/.content-comment > .full_content,.title-detail,/*just dantri.com.vn*/.e-magazine__title,.detail.title-page{font-size:22px;}');
