6b2ed1d256534781b709ff2c619d3ab3f34b173e
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2024-07-20 14:02:55 +0700

    Update YouTube H.264 + FPS.user.js

--- YouTube H.264 + FPS.user.js
+++ YouTube H.264 + FPS.user.js
@@ -2,7 +2,7 @@
 // @name YouTube H.264 + FPS
 // @name:ru YouTube H.264 + FPS
 // @namespace https://www.youtube.com
-// @version     1.2
+// @version     1.3
 // @description Clone of h264ify with optional limit up to 30 FPS.
 // @description:ru Клон h264ify с опциональным ограничением до 30 FPS.
 // @match *://*.youtube.com/*
