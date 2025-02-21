522110b1b7e83ebbf8a4ebdab130e9f29b51a498
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2025-02-21 15:32:22 +0700

    Update yt-h264.user.js

--- yt-h264.user.js
+++ yt-h264.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube H.264
 // @namespace    https://www.youtube.com
-// @version      1.3.2
+// @version      1.3.3
 // @description  Clone of h264ify with optional limit up to 30 FPS.
 // @description:ru Клон h264ify с опциональным ограничением до 30 FPS.
 // @match        *://*.youtube.com/*
@@ -12,35 +12,43 @@
 // @run-at       document-start
 // ==/UserScript==
 
-// Constants for video settings
-const BLOCK_HIGH_FPS = false;
-const DISALLOWED_TYPES_REGEX = /webm|vp8|vp9|av01/i;
-const FRAME_RATE_REGEX = /framerate=(\d+)/;
-
 (function() {
-    const mediaSource = window.MediaSource;
+    const DISALLOWED_TYPES_REGEX = /webm|vp8|vp9|av01/i;
 
-    if (!mediaSource) {
-        console.warn('MediaSource API is not available.');
-        return;
+    function overrideIsTypeSupported() {
+        const originalIsTypeSupported = MediaSource.isTypeSupported.bind(MediaSource);
+
+        MediaSource.isTypeSupported = (type) => {
+            if (typeof type !== 'string') return false;
+
+            // Block disallowed types
+            if (DISALLOWED_TYPES_REGEX.test(type)) return false;
+
+            return originalIsTypeSupported(type);
+        };
     }
 
-    const originalIsTypeSupported = mediaSource.isTypeSupported.bind(mediaSource);
+    function blockMediaSourceExtensions() {
+        const originalIsTypeSupported = window.MediaSource.isTypeSupported;
+        window.MediaSource.isTypeSupported = function(type) {
+            if (DISALLOWED_TYPES_REGEX.test(type)) {
+                return false;
+            }
+            return originalIsTypeSupported.apply(this, arguments);
+        };
 
-    mediaSource.isTypeSupported = (type) => {
-        // Ensure type is a string
-        if (typeof type !== 'string') return false;
-
-        // Block disallowed types
-        if (DISALLOWED_TYPES_REGEX.test(type)) return false;
-
-        // Extract and limit frame rate if needed
-        const frameRateMatch = FRAME_RATE_REGEX.exec(type);
-        if (frameRateMatch && parseInt(frameRateMatch[1], 10) > 30) {
-            return false;
+        if ('SourceBuffer' in window) {
+            const originalIsTypeSupported = window.SourceBuffer.prototype.appendBuffer;
+            window.SourceBuffer.prototype.appendBuffer = function(data) {
+                const type = this.mimeType;
+                if (DISALLOWED_TYPES_REGEX.test(type)) {
+                    throw new Error('Disallowed type');
+                }
+                return originalIsTypeSupported.apply(this, arguments);
+            };
         }
+    }
 
-        // Use original isTypeSupported method for other cases
-        return originalIsTypeSupported(type);
-    };
+    overrideIsTypeSupported();
+    blockMediaSourceExtensions();
 })();
