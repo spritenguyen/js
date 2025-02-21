e54210b49091a336d7fbd7b939ab832989eb158c
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2025-02-21 15:35:54 +0700

    Update yt-h264.user.js

--- yt-h264.user.js
+++ yt-h264.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube H.264
 // @namespace    https://www.youtube.com
-// @version      1.3.3
+// @version      1.3.4
 // @description  Clone of h264ify with optional limit up to 30 FPS.
 // @description:ru Клон h264ify с опциональным ограничением до 30 FPS.
 // @match        *://*.youtube.com/*
@@ -13,42 +13,33 @@
 // ==/UserScript==
 
 (function() {
-    const DISALLOWED_TYPES_REGEX = /webm|vp8|vp9|av01/i;
+    'use strict';
 
-    function overrideIsTypeSupported() {
-        const originalIsTypeSupported = MediaSource.isTypeSupported.bind(MediaSource);
+    const DISALLOWED_CODECS = ['vp9', 'av01'];
 
-        MediaSource.isTypeSupported = (type) => {
-            if (typeof type !== 'string') return false;
+    const originalCanPlayType = HTMLVideoElement.prototype.canPlayType;
 
-            // Block disallowed types
-            if (DISALLOWED_TYPES_REGEX.test(type)) return false;
-
-            return originalIsTypeSupported(type);
-        };
-    }
-
-    function blockMediaSourceExtensions() {
-        const originalIsTypeSupported = window.MediaSource.isTypeSupported;
-        window.MediaSource.isTypeSupported = function(type) {
-            if (DISALLOWED_TYPES_REGEX.test(type)) {
-                return false;
-            }
-            return originalIsTypeSupported.apply(this, arguments);
-        };
-
-        if ('SourceBuffer' in window) {
-            const originalIsTypeSupported = window.SourceBuffer.prototype.appendBuffer;
-            window.SourceBuffer.prototype.appendBuffer = function(data) {
-                const type = this.mimeType;
-                if (DISALLOWED_TYPES_REGEX.test(type)) {
-                    throw new Error('Disallowed type');
-                }
-                return originalIsTypeSupported.apply(this, arguments);
-            };
+    HTMLVideoElement.prototype.canPlayType = function(type) {
+        if (DISALLOWED_CODECS.some(codec => type.includes(codec))) {
+            return '';
         }
-    }
+        return originalCanPlayType.call(this, type);
+    };
 
-    overrideIsTypeSupported();
-    blockMediaSourceExtensions();
+    const observer = new MutationObserver(() => {
+        const videos = document.querySelectorAll('video');
+
+        videos.forEach(video => {
+            const originalAddSourceBuffer = video.addSourceBuffer;
+
+            video.addSourceBuffer = function(type) {
+                if (DISALLOWED_CODECS.some(codec => type.includes(codec))) {
+                    throw new Error('Disallowed codec');
+                }
+                return originalAddSourceBuffer.call(this, type);
+            };
+        });
+    });
+
+    observer.observe(document, { childList: true, subtree: true });
 })();
