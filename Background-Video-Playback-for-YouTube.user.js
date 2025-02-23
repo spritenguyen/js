8d88d78514d865d795124a87479a71d99b0ddb85
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2025-02-23 16:32:32 +0700

    Update Background-Video-Playback-for-YouTube.user.js

--- Background-Video-Playback-for-YouTube.user.js
+++ Background-Video-Playback-for-YouTube.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Background Audio Playback for YouTube
+// @name         Picture-in-Picture for YouTube
 // @namespace    https://yournamespacehere.com
-// @version      1.1
-// @description  Enable background audio playback on YouTube for Edge Android
+// @version      1.0
+// @description  Enable Picture-in-Picture mode for YouTube on Edge Android
 // @author       YourName
 // @match        *://*.youtube.com/*
 // @grant        none
@@ -11,31 +11,29 @@
 (function() {
     'use strict';
 
-    let videoElement = null;
-
-    function enableBackgroundAudio() {
-        if (videoElement) {
-            // Pause and play to ensure video resumes correctly
-            videoElement.pause();
-            setTimeout(() => {
-                videoElement.play().catch(() => {});
-            }, 100);
+    function enterPictureInPicture(video) {
+        if (document.pictureInPictureEnabled && !document.pictureInPictureElement) {
+            video.requestPictureInPicture().catch(err => {
+                console.error(`Failed to enter Picture-in-Picture mode: ${err}`);
+            });
         }
     }
 
     function onVideoPlay(event) {
         if (event.target.tagName === 'VIDEO') {
-            videoElement = event.target;
-            enableBackgroundAudio();
-        }
-    }
+            const video = event.target;
+            video.addEventListener('enterpictureinpicture', () => {
+                console.log('Entered Picture-in-Picture mode.');
+            });
+            video.addEventListener('leavepictureinpicture', () => {
+                console.log('Exited Picture-in-Picture mode.');
+            });
 
-    function onVisibilityChange() {
-        if (document.hidden && videoElement) {
-            enableBackgroundAudio();
+            setTimeout(() => {
+                enterPictureInPicture(video);
+            }, 3000); // Delay to allow the video to start playing
         }
     }
 
     document.addEventListener('play', onVideoPlay, true);
-    document.addEventListener('visibilitychange', onVisibilityChange);
 })();
