a1c959f244fe2cbea18ba29cbb53c19db7820ad4
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2025-02-23 16:37:50 +0700

    Update Background-Video-Playback-for-YouTube.user.js

--- Background-Video-Playback-for-YouTube.user.js
+++ Background-Video-Playback-for-YouTube.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Picture-in-Picture for YouTube
+// @name         Background Audio Playback for YouTube on Android
 // @namespace    https://yournamespacehere.com
-// @version      1.0
-// @description  Enable Picture-in-Picture mode for YouTube on Edge Android
+// @version      1.2
+// @description  Enable background audio playback on YouTube for Edge Android
 // @author       YourName
 // @match        *://*.youtube.com/*
 // @grant        none
@@ -11,29 +11,28 @@
 (function() {
     'use strict';
 
-    function enterPictureInPicture(video) {
-        if (document.pictureInPictureEnabled && !document.pictureInPictureElement) {
-            video.requestPictureInPicture().catch(err => {
-                console.error(`Failed to enter Picture-in-Picture mode: ${err}`);
-            });
+    let videoElement = null;
+
+    function enableBackgroundAudio() {
+        if (videoElement) {
+            // Play the video to ensure audio playback continues in the background
+            videoElement.play().catch(() => {});
         }
     }
 
     function onVideoPlay(event) {
         if (event.target.tagName === 'VIDEO') {
-            const video = event.target;
-            video.addEventListener('enterpictureinpicture', () => {
-                console.log('Entered Picture-in-Picture mode.');
-            });
-            video.addEventListener('leavepictureinpicture', () => {
-                console.log('Exited Picture-in-Picture mode.');
-            });
+            videoElement = event.target;
+            enableBackgroundAudio();
+        }
+    }
 
-            setTimeout(() => {
-                enterPictureInPicture(video);
-            }, 3000); // Delay to allow the video to start playing
+    function onVisibilityChange() {
+        if (document.hidden && videoElement) {
+            enableBackgroundAudio();
         }
     }
 
     document.addEventListener('play', onVideoPlay, true);
+    document.addEventListener('visibilitychange', onVisibilityChange);
 })();
