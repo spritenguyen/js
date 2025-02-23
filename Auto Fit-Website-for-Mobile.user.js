26f4b8bf439589796d03095499d00412bf9830be
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2025-02-23 09:29:38 +0700

    Create Auto Fit-Website-for-Mobile.user.js

---
+++ Auto Fit-Website-for-Mobile.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Auto Fit Website for Mobile
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Adjust website layout for mobile screens
+// @author       Your Name
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Function to fit the website to the mobile screen
+    function fitWebsiteForMobile() {
+        const viewportMeta = document.createElement('meta');
+        viewportMeta.name = 'viewport';
+        viewportMeta.content = 'width=device-width, initial-scale=1.0';
+        document.getElementsByTagName('head')[0].appendChild(viewportMeta);
+
+        document.body.style.maxWidth = '100%';
+        document.body.style.width = '100%';
+        document.body.style.overflowX = 'hidden';
+    }
+
+    // Run the function on page load
+    window.addEventListener('load', fitWebsiteForMobile);
+})();
