be0bbddf61eef1fed849109a2e00b9daec342c69
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2025-02-23 15:35:01 +0700

    Update Keep-Screen-On-for-YouTube.user.js

--- Keep-Screen-On-for-YouTube.user.js
+++ Keep-Screen-On-for-YouTube.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Keep Screen On for YouTube
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.2
 // @description  Giữ màn hình luôn sáng khi xem video trên YouTube
 // @author       Your Name
 // @match        https://www.youtube.com/*
@@ -11,57 +11,37 @@
 (function() {
     'use strict';
 
-    let wakeLock = null;
-
-    const requestWakeLock = async () => {
-        try {
-            if (!wakeLock) {
-                wakeLock = await navigator.wakeLock.request('screen');
-                console.log('Wake Lock is active');
-            }
-        } catch (err) {
-            console.error(`${err.name}, ${err.message}`);
-        }
-    };
-
-    const releaseWakeLock = async () => {
-        if (wakeLock !== null) {
-            try {
-                await wakeLock.release();
-                wakeLock = null;
-                console.log('Wake Lock is released');
-            } catch (err) {
-                console.error(`${err.name}, ${err.message}`);
-            }
-        }
-    };
-
-    const manageWakeLock = () => {
-        const video = document.querySelector('video');
+    function keepScreenOn() {
+        let video = document.querySelector('video');
         if (video) {
-            if (!video.paused) {
-                requestWakeLock();
-            } else {
-                releaseWakeLock();
-            }
-        }
-    };
+            let wakeLock = null;
 
-    const setupWakeLock = () => {
-        const video = document.querySelector('video');
-        if (video) {
+            const requestWakeLock = async () => {
+                try {
+                    wakeLock = await navigator.wakeLock.request('screen');
+                } catch (err) {
+                    console.error(`${err.name}, ${err.message}`);
+                }
+            };
+
+            const releaseWakeLock = () => {
+                if (wakeLock !== null) {
+                    wakeLock.release()
+                        .then(() => {
+                            wakeLock = null;
+                        });
+                }
+            };
+
             video.addEventListener('play', requestWakeLock);
             video.addEventListener('pause', releaseWakeLock);
             video.addEventListener('ended', releaseWakeLock);
         }
-    };
+    }
 
-    setupWakeLock();
-    document.addEventListener('fullscreenchange', manageWakeLock);
+    let observer = new MutationObserver(() => {
+        keepScreenOn();
+    });
 
-    const observer = new MutationObserver(setupWakeLock);
     observer.observe(document.body, { childList: true, subtree: true });
-
-    window.addEventListener('beforeunload', releaseWakeLock);
-
 })();
