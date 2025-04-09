4fcce954bc17023d90b153d29af9787886719563
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2025-04-09 18:20:48 +0700

    Update chan-font.user.js

--- chan-font.user.js
+++ chan-font.user.js
@@ -1,18 +1,31 @@
 // ==UserScript==
-// @name         Custom Font Blocker
+// @name         Font Remover for MSN
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Block specific fonts on msn.com
-// @author       
-// @match        *://*/*
+// @version      1.1
+// @description  Remove custom fonts on msn.com
+// @author       You
+// @match        *://www.msn.com/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // Thực hiện việc loại bỏ fonts bằng cách sử dụng các phương thức JavaScript
-    document.querySelectorAll('*').forEach(el => {
-        el.style.fontFamily = 'inherit';
+    // Hàm thay đổi font trên tất cả các phần tử
+    const removeCustomFonts = () => {
+        document.querySelectorAll('*').forEach(el => {
+            el.style.fontFamily = 'inherit'; // Đặt font về mặc định
+        });
+    };
+
+    // Thực hiện lần đầu khi trang được tải
+    removeCustomFonts();
+
+    // Quan sát các thay đổi DOM để áp dụng lại nếu có phần tử mới xuất hiện
+    const observer = new MutationObserver(() => {
+        removeCustomFonts();
     });
+
+    // Bắt đầu quan sát toàn bộ body của trang
+    observer.observe(document.body, { childList: true, subtree: true });
 })();
