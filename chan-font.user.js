2c5ecda8291abd63ec7955b6839d62a2d19aed5e
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2025-04-09 20:11:31 +0700

    Update chan-font.user.js

--- chan-font.user.js
+++ chan-font.user.js
@@ -1,33 +1,13 @@
 // ==UserScript==
-// @name         Block Font Downloads on MSN
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Block font downloads from msn.com
-// @author       You
-// @match        *://www.msn.com/*
-// @grant        none
+// @name         Block Fonts on msn.com
+// @match        *://*.msn.com/*
+// @run-at       document-start
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    // Sử dụng interception để chặn các yêu cầu tải font
-    const originalFetch = window.fetch;
-    window.fetch = function(...args) {
-        if (args[0] && args[0].includes('.woff') || args[0].includes('.ttf') || args[0].includes('.otf')) {
-            console.log('Blocked font fetch request:', args[0]);
-            return Promise.resolve(new Response(null, { status: 403 }));
-        }
-        return originalFetch.apply(this, args);
-    };
-
-    const originalOpen = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function(method, url, ...rest) {
-        if (url.includes('.woff') || url.includes('.ttf') || url.includes('.otf')) {
-            console.log('Blocked font XHR request:', url);
-            this.abort();
-        } else {
-            originalOpen.apply(this, [method, url, ...rest]);
-        }
-    };
-})();
+const blockFonts = () => {
+    const css = "* { font-family: Arial, sans-serif !important; }";
+    const style = document.createElement('style');
+    style.textContent = css;
+    document.head.appendChild(style);
+};
+blockFonts();
