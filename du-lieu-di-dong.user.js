d80b83e1aed72adb9e9f97572faad1929c0c5d04
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2025-04-11 19:36:26 +0700

    Update du-lieu-di-dong.user.js

--- du-lieu-di-dong.user.js
+++ du-lieu-di-dong.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Chặn Tải Hình Ảnh Trên Mạng Di Động (V2)
+// @name         Chặn Tải Hình Ảnh Trên Mạng Di Động (V3)
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Ngăn tải hình ảnh nếu sử dụng dữ liệu di động và hiển thị thông báo gọn trong khung ảnh
 // @author       Nguyen
 // @match        *://*/*
@@ -38,9 +38,10 @@
                 display: flex;
                 align-items: center;
                 justify-content: center;
+                overflow: hidden;
                 text-align: center;
                 color: red;
-                font-size: 14px;
+                font-size: calc(${Math.min(width, height)} / 10); /* Kích thước chữ thay đổi phù hợp với khung */
                 border: 1px solid #ccc;
                 background-color: #f9f9f9;
                 box-sizing: border-box;
